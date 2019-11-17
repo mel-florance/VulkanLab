9d4ad8a838f29e341a1e362396a600fedd4accc9
Mel Florance <mel.florance@gmail.com>
2019-11-17 12:03:02 +0100

    Update project utility scripts.

--- setup.sh
+++ setup.sh
@@ -1,1 +1,9 @@
+<<<<<<< Updated upstream
 cmake . && make clean && make -j4 && ./bin/VulkanLab
+=======
+cmake . \
+&& make clean \
+&& make -j4 \
+&& cd bin/ \
+&& ./VulkanLab
+>>>>>>> Stashed changes
